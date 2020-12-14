b833ea1afcd348cbc69940ebca08af0ef16c40a0
xiaozhuoyue <xiaozhuoyue@momenta.ai>
2020-12-14 14:50:50 +0800

    update

--- examples/applications/author-disambiguation/clustering.py
+++ examples/applications/author-disambiguation/clustering.py
@@ -252,7 +252,6 @@ def clustering(input_signatures, input_records, distance_model,
     else:
         y = None
     '''
-    print(true_clusters)
     clusterer = BlockClustering(
         # blocking=block_function,
         base_estimator=ScipyHierarchicalClustering(
@@ -266,6 +265,7 @@ def clustering(input_signatures, input_records, distance_model,
 
     pres = clusterer.labels_
     truth = {sig: clu for clu, sigs in true_clusters.items() for sig in sigs}
+    truth = [truth[sig] for sig_i, sig in enumerate(sorted(truth.keys()))]
     result = pairwise_precision_recall_f1(truth, pres)
     if output_clusters:
         with open(output_clusters, 'w') as f:
